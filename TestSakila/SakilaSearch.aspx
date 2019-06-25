087318a4a40afa6b74c56550391b8dc6a252d6c7
Pain-au-chocllat <samuel18081988@hotmail.co.uk>
2019-06-25 11:22:16 +0100

    Initial commit

---
+++ TestSakila/SakilaSearch.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SakilaSearch.aspx.cs" Inherits="TestSakila.SakilaSearch" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <link rel="stylesheet" href="StyleSheet.css" />
+    <title>Gookila</title>
+</head>
+<body class="centered-wrapper">
+    <form id="form1" runat="server">
+        <div class="centered-content">
+           <asp:Image ID="Image1" runat="server" ImageUrl="~/gookila4.png"></asp:Image>
+           <hr />
+           <asp:DropDownList ID="ActorList" runat="server" AppendDataBoundItems="true">
+               <asp:ListItem Text="Select Actor" Value="0" />
+           </asp:DropDownList>               
+           <asp:Button ID="SearchButton" BorderStyle="None" runat="server" Text="Search" OnClick="SearchButton_Click" />
+           <hr />
+           <asp:GridView CssClass="centered-content" ID="GridView1" runat="server" BorderStyle="None" ForeColor="White" ShowHeaderWhenEmpty="true" EmptyDataText="You will need to select an actor first">
+           </asp:GridView>
+           <hr />
+           <asp:Label ID="ExceptionLabel" runat="server" style="color:white" Text="This is where exceptions go! You're all good!"></asp:Label>
+            <hr />
+        </div>
+    </form>
+</body>
+</html>
---
+++ TestSakila/SakilaSearch.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace TestSakila
+{
+    public partial class SakilaSearch : System.Web.UI.Page
+    {
+
+        string dbConn = ConfigurationManager.ConnectionStrings["SakilaConnection"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataTable actors = new DataTable();
+
+            //Populate the drop down list
+            if (ActorList.Items.Count == 1)
+            {
+                using (SqlConnection con = new SqlConnection(dbConn))
+                {
+                    try
+                    {
+                        SqlDataAdapter adapter = new SqlDataAdapter("SELECT CONCAT(first_name, ' ', last_name) AS Result FROM actor", con);
+                        adapter.Fill(actors);
+
+                        ActorList.DataSource = actors;
+                        ActorList.DataTextField = "Result";
+                        ActorList.DataBind();
+                    }
+                    catch (SqlException ex)
+                    {
+                        ExceptionLabel.Text = "Unable to connect to Sakila database! Error message:  " + ex.Message;
+                    }
+                }
+            }
+        }
+
+        protected void SearchButton_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+
+            using (SqlConnection sqlCon = new SqlConnection(dbConn))
+            {
+                try
+                {
+                    //Query to display films from selected actor
+
+                    SqlCommand sqlCom = new SqlCommand($@"SELECT a.first_name, a.last_name, fi.title, COUNT(*) as pop
+                                                            FROM actor as a 
+                                                            INNER JOIN film_actor as fa ON a.actor_id = fa.actor_id 
+                                                            INNER JOIN film as fi ON fi.film_id = fa.film_id
+                                                            INNER JOIN inventory as i ON fi.film_id = i.film_id
+                                                            INNER JOIN rental as r ON r.inventory_id = i.inventory_id 
+                                                            WHERE a.actor_id = @actorList
+                                                            GROUP BY a.first_name, a.last_name, fi.title
+                                                            HAVING COUNT(*) > 0
+                                                            ORDER BY pop DESC", sqlCon);
+
+                    sqlCom.Parameters.AddWithValue("@actorList", ActorList.SelectedIndex);
+
+                    SqlDataAdapter sda = new SqlDataAdapter(sqlCom);
+                    sda.Fill(dt);
+
+                    //Sort datatable the bind to grid view
+                    GridView1.DataSource = dt;
+                    GridView1.DataBind();
+
+                }
+                catch (SqlException ex)
+                {
+                    ExceptionLabel.Text = "Unable to connect to Sakila database! Error message:  " + ex.Message;
+                }
+            }
+        }         
+    }
+}
